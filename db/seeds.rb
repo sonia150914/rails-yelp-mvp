cd84b1e47f03199e7c233604b322028764806e09
sonia jaunin <sonia_jaunin@hotmail.com>
2021-11-17 16:35:26 +0100

    add

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,15 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  bacio = Restaurant.create(name: 'Bacio', address: 'Renens', phone_number: '021 634 87 87', category: 'italian')
+  #bacio.review = 'Très bon pour les repas de midi. Personnel agréable.'
+
+  pyramide = Restaurant.create(name: 'Restaurant la Pyramide', address: 'Rte de Chavannes 35, 1007 Lausanne', phone_number: '021 625 14 48', category: 'french',)
+  #pyramide.review = 'Nous avons bien mangé, mais le cadre n\'est pas idéal pour un repas en amoureux, il est plus adapté à un repas entre collègues...'
+
+  tilleul = Restaurant.create(name: 'Café des Tilleuls', address: 'Rue de Lausanne 52, 1020 Renens', phone_number: '058 300 19 59', category: 'chinese')
+  #tilleul.review = 'Magnifique endroit, belle terrasse et gens accueillant'
+  brazil = Restaurant.create(name: 'Le Brazil Beef Grill', address: 'Rue du Caudray 2, 1020 Renens', phone_number: '021 635 71 15', category: 'japanese')
+  rose = Restaurant.create(name: 'Rose Rouge', address: 'Av. du Temple 2, 1020 Renens', phone_number: '021 634 58 27', category: 'french')
+
+  puts 'finished'
